f6be046c3678161549968476c529a8aa142ab84a
thalesacardoso <thales.dev@hotmail.com>
2023-08-28 10:13:53 -0300

    estrutura condicional 1.0

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Desafio de projeto 01
+
+// variáveis
+let nomeHeroi = "Super-man"
+let xpHeroi = 1500
+let nivelHeroi
+
+if (xpHeroi < 1000){
+    nivelHeroi = "Ferro"
+} else if(xpHeroi >= 1001 && xpHeroi <= 2000) {
+    nivelHeroi = "Bronze"
+}
+
+
+console.log("O seu herói está no nível " + nivelHeroi)
+
